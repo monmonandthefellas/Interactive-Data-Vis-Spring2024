ec69761c77d6726c97b0e0158d61496cf332d733
Monica <monicharocha@gmail.com>
2024-02-05 17:53:43 -0500

    create button and counter

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,8 @@
 console.log('hello world');
+
+let htmlButton = document.querySelector('button');
+let myCounter = document.getElementById('counter');
+
+function changeCounter() {
+   myCounter.innerText++;
+} 
