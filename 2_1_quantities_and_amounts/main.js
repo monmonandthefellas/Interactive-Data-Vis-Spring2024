69bf16a5db57ef0f6efe0ad87d703af9f03df6b6
Monica <monicharocha@gmail.com>
2024-03-24 18:22:29 -0400

    added two rows for nationalities and columns

--- 2_1_quantities_and_amounts/main.js
+++ 2_1_quantities_and_amounts/main.js
@@ -18,12 +18,48 @@ d3.csv('../data/MoMA_topTenNationalities.csv', d3.autoType)
     const container = d3.select("#container")
     console.log(container)
 
-    container
-      .selectAll(".data")
-      .data(data)
-      .join("div")
-      .attr("class", "data")
-      .text((d) => d.nationality)
-  })
+    // container
+    //   .selectAll(".data")
+    //   .data(data)
+    //   .join("div")
+    //   .attr("class", "count")
+    //   .text((d) => d.Nationality)
+ 
 
-  
+    const header = container
+    .append("th")
+    .style("background-color", "yellow")
+    .text("Nationalities represented in the MoMa collection")
+
+  const table = container
+    .append("table")
+    .attr("class", "data")
+
+
+    const firstRow = table
+    .append("tr")
+    .attr("class","firstRow")
+
+    firstRow.selectAll("td.cell")
+    .data(data)
+    .join("td")
+    .attr("class","cell")
+    .text((d) => d.Nationality)
+    
+
+    const secondRow = table
+    .append("tr")
+    .attr("class", "secondRow")
+
+    secondRow.selectAll("td.cell")
+    .data(data)
+    .join("td")
+    .attr("class","cell")
+    .text((d) => d.Count)
+    .style("color", "blue")
+    .style("width", (d) => {
+      return 100 + "px";
+    }) 
+    
+
+  })
