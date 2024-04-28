6ccf7c7c7e07072abb6d0e2e040edf7c411c0979
Monica <monicharocha@gmail.com>
2024-04-28 18:17:18 -0400

    finished tutorials

--- 2_2_distributions/main.js
+++ 2_2_distributions/main.js
@@ -11,13 +11,92 @@ d3.json("../data/environmentRatings.json", d3.autoType).then(data => {
     // xscale - linear.count
     const xScale = d3.scaleLinear()
     .domain([0,1])
-    .range([])
+    .range([margin.left, width - margin.right])
+
+    //yscale
+    const yScale = d3.scaleLinear()
+    .domain([0,100])
+    .range([height - margin.bottom, margin.top])
     
+    // color scale
+
+    const colorScale = d3.scaleOrdinal()
+    .domain(["D", "R"])
+    .range(["blue", "red"])
     /* HTML ELEMENTS */
   //svg
-  const svg = d3.select(#container)
+  const svg = d3.select("#container")
   .append("svg")
   .attr("width", width)
   .attr("height", height)
+
+  //axis scale
+
+  const xAxis = d3.axisBottom(xScale);
+  svg.append("g")
+    .attr("transform", `translate(0, ${height - margin.bottom})`)
+    .call(xAxis);
+
+  const yAxis = d3.axisLeft(yScale);
+  svg.append("g")
+    .attr("transform", `translate(${margin.left}, 0)`)
+    .call(yAxis);
+
+
+  /* Draw Circles */
+//   svg.selectAll(".circle")
+//   .data(data)
+//   .join("circle")
+//   .attr("cx", (d, i) => xScale(d.ideologyScore2020))
+//   .attr("cy", (d, i) => yScale(d.envScore2020))
+//   .attr("r", radius)
+// .attr("fill", d => d.Party === "R" ? "red" : "blue" )
+
+// 1. grow from a radius of 0 to a radius of radius
+
+// svg.selectAll(".circle")
+// .data(data)
+// .join("circle")
+// .attr("cx", (d, i) => xScale(d.ideologyScore2020))
+// .attr("cy", (d, i) => yScale(d.envScore2020))
+// .attr("r", 0) //initial value
+// .attr("fill", d => d.Party === "R" ? "red" : "blue" )
+
+
+// .call(sel => sel.transition()
+//   .delay(500)
+//   .duration(1500)
+//   .attr("r",radius )) //final value
+
+  // 2. change from black to its given color
+
+//   svg.selectAll(".circle")
+// .data(data)
+// .join("circle")
+// .attr("cx", (d, i) => xScale(d.ideologyScore2020))
+// .attr("cy", (d, i) => yScale(d.envScore2020))
+// .attr("r", radius) 
+// .attr("fill", "black")
+
+// .call(sel => sel.transition()
+//   .delay(500)
+//   .duration(1500)
+//   .attr("fill", d => colorScale(d.Party) )) //final value
+
+  // 3. Move from bottom up, left to right, top to bottom, right to left
     
+  svg.selectAll(".circle")
+  .data(data)
+  .join("circle")
+  .attr("cx", (width/2))
+  .attr("cy", (d, i) => yScale(d.envScore2020))
+  .attr("r", radius) 
+  .attr("fill", d => colorScale(d.Party))
+  
+  .call(sel => sel.transition()
+    .delay(500)
+    .duration(1500)
+    .attr("cx", (d, i) => xScale(d.ideologyScore2020))
+   // .attr("cy", (d, i) => yScale(d.envScore2020))
+  )
   });
--- 2_3_time_series/main.js
+++ 2_3_time_series/main.js
@@ -1,26 +1,71 @@
  /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ;
+const width = window.innerWidth * 0.7,
+  height = window.innerHeight * 0.7,
+  margin = { top: 20, bottom: 50, left: 60, right: 60 };
 
 /* LOAD DATA */
-d3.csv('[PATH_TO_YOUR_DATA]', d => {
+d3.csv('../data/populationOverTime.csv', d => {
   return {
-    // year: new Date(+d.Year, 0, 1),
-    // country: d.Entity,
-    // population: +d.Population
+    year: new Date(+d.Year, 0, 1),
+    country: d.Entity,
+    population: +d.Population
   }
 }).then(data => {
   console.log('data :>> ', data);
 
   // SCALES
+  const xScale = d3.scaleTime()
+  .domain(d3.extent(data, d => d.year))
+  .range([margin.left, width - margin.right])
+
+  const yScale = d3.scaleLinear()
+  .domain(d3.extent(data, d => d.population))
+  .range([height - margin.bottom, margin.top])
 
   // CREATE SVG ELEMENT
+  const svg = d3.select("#container")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height)
 
   // BUILD AND CALL AXES
 
   // LINE GENERATOR FUNCTION
+  const lineGen = d3.line()
+  .x(d => xScale(d.year))
+  .y(d => yScale(d.population))
 
   // DRAW LINE
+const afghanistan = data.filter( d => d.country === "Afghanistan")
+const hungary = data.filter( d => d.country === "Hungary")
+const china = data.filter( d => d.country === "China")
 
+
+// svg.selectAll(".circle")
+// .data(afghanistan)
+// .join("circle")
+// .attr("class", "circle")
+// .attr("cx", d => xScale(d.year))
+// .attr("cx", d => xScale(d.population))
+// .attr("r", 1)
+
+// svg.selectAll(".country")
+// .data([afghanistan, hungary, china])
+// .join("path")
+// .attr("class", "afghanistan")
+// .attr("fill", "none")
+// .attr("stroke", "black")
+// .attr("d", d => lineGen(d))
+// 
+
+const groupedData = d3.groups(data, d => d.country)
+console.log(groupedData)
+
+svg.selectAll("path.country")
+.data(groupedData)
+.join("path")
+.attr("class", "country")
+.attr("d", ([countryName, data]) => lineGen(data))
+.attr("fill", "none")
+.attr("stroke", "black")
 });
