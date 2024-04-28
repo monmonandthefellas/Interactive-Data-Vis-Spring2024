3c661b0213e6cb3f7d7c11922ed76b0175de3155
Monica <monicharocha@gmail.com>
2024-04-28 16:15:02 -0400

    adding boilerplate

--- 2_2_distributions/main.js
+++ 2_2_distributions/main.js
@@ -1,16 +1,23 @@
-/* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ,
-//   radius = ;
+const width = window.innerWidth * 0.7,
+  height = window.innerHeight * 0.7,
+  margin = { top: 20, bottom: 60, left: 60, right: 40 },
+  radius = 5;
 
 /* LOAD DATA */
-d3.csv("../data/MoMA_distributions.csv", d3.autoType)
-  .then(data => {
-    console.log(data)
+d3.json("../data/environmentRatings.json", d3.autoType).then(data => {
+  console.log(data)
 
     /* SCALES */
+    // xscale - linear.count
+    const xScale = d3.scaleLinear()
+    .domain([0,1])
+    .range([])
     
     /* HTML ELEMENTS */
+  //svg
+  const svg = d3.select(#container)
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height)
     
   });
